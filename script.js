e9e6f6c4413e2152364db4ad384e5cca22750c25
Rico McCoy <68340447+rmccoy2k@users.noreply.github.com>
2021-06-13 00:59:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,41 +14,54 @@
 
 window.onload = () => {
   setTimeout(() => {
-    document.querySelector("body").classList.add("display");
+    document.querySelector('body').classList.add('display');
   }, 4000);
 };
 
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
-  document.querySelector(".container").classList.toggle("change");
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+  document.querySelector('.container').classList.toggle('change');
 });
 
 // Scroll Button
-document.querySelector(".scroll-btn").addEventListener("click", () => {
-  document.querySelector("html").style.scrollBehavior = "smooth";
+document.querySelector('.scroll-btn').addEventListener('click', () => {
+  document.querySelector('html').style.scrollBehavior = 'smooth';
   setTimeout(() => {
-    document.querySelector("html").style.scrollBehavior = "unset";
+    document.querySelector('html').style.scrollBehavior = 'unset';
   }, 1000);
 });
 
 // Auto Text
-const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = "Welcome! Rico McCoy is a Frontend & Backend Developer."
-let idx = 1
-let speed = 300 / speedEl.value
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'Welcome! Rico McCoy is a Frontend & Backend Developer.';
+let idx = 1;
+let speed = 300 / speedEl.value;
 
-writeText()
+writeText();
 
 function writeText() {
-  textEl.innerText = text.slice(0, idx)
+  textEl.innerText = text.slice(0, idx);
 
-  idx++
+  idx++;
 
-  if(idx > text.length) {
-    idx = 1
+  if (idx > text.length) {
+    idx = 1;
   }
-  setTimeout(writeText, speed)
-} 
+  setTimeout(writeText, speed);
+}
 
-speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+speedEl.addEventListener('input', (e) => (speed = 300 / e.target.value));
 // End of Auto Text
+
+// Check Email
+const checkEmail = (input) => {
+  const regEx =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regEx.test(input.value.trim())) {
+    success(input);
+  } else {
+    error(input, 'Email is not valid');
+  }
+};
+// End of Check Email
